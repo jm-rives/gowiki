be95e09848c16e46ec1e04eddffca3f8ed1a1db3
jm-rives <joanna.rives@gmail.com>
2017-05-31 20:22:26 -0700

    ADD Test Function

--- wiki.go
+++ wiki.go
@@ -25,3 +25,9 @@ func loadPage(title string) *Page {
 
 }
 
+func main() {
+	p1 :=$Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
