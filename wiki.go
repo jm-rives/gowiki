06cb488c0e5568eb29a18c3211866b069d430790
jm-rives <joanna.rives@gmail.com>
2017-05-31 20:23:25 -0700

    FIX Typo

--- wiki.go
+++ wiki.go
@@ -26,7 +26,7 @@ func loadPage(title string) *Page {
 }
 
 func main() {
-	p1 :=$Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
