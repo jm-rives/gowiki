3936cee4ece9530ce7b398210ce813a2e2097254
jm-rives <joanna.rives@gmail.com>
2017-05-31 20:18:11 -0700

    ADD error handling

--- wiki.go
+++ wiki.go
@@ -17,7 +17,11 @@ func (p *Page) save () error {
 
 func loadPage(title string) *Page {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := ioutil.ReadFile(filename)
+	if err!= nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 
 }
+
