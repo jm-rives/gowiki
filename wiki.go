a6ef0a0e81ce056665c61e65c9a37f1e07291f75
jm-rives <joanna.rives@gmail.com>
2017-05-31 20:02:11 -0700

    ADD Load page function

--- wiki.go
+++ wiki.go
@@ -15,3 +15,9 @@ func (p *Page) save () error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+
+}
